6d9524d7e45d72d5ce23e683a9cc830075743000
Robert Yang <yang.robert.w@gmail.com>
2015-09-18 15:10:54 -0400

    added week 3 (maya test, UI button and UI sliders)

---
+++ week03/Assets/scripts/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI; // we need this line to talk to UI
+
+public class ButtonTest : MonoBehaviour {
+
+	int points = 0;
+	public Text pointsDisplay; // assign in Inspector
+
+	// a function must be "public void" in order for UI to call it
+	public void EarnAPoint () {
+		points++; // add 1 to existing value
+		pointsDisplay.text = "YOU HAVE: " + points.ToString() + " points!";
+	}
+
+
+}
---
+++ week03/Assets/scripts/SliderTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// use a Slider to rotate a Cube (admire the cube from all angles)
+public class SliderTest : MonoBehaviour {
+
+	public Transform cube;
+
+	// UI functions must be PUBLIC VOID
+	public void SetCubeAngle ( float yAngle ) {
+		// "OY-ler", 0-360 degree angles
+		cube.eulerAngles = new Vector3( 0f, yAngle, 0f );
+		// do NOT use "cube.rotation" / "transform.rotation"
+	}
+
+}
