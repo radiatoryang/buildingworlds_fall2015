4a218051e85b34aa9cbe8d8491cba34090c3f522
Robert Yang <yang.robert.w@gmail.com>
2015-11-20 18:26:18 -0500

    week11: procedural mesh deformation (water waves)

---
+++ week11/Assets/scripts/OceanDeform.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OceanDeform : MonoBehaviour {
+
+	// the MeshFilter tells Unity what mesh we're using
+	MeshFilter mFilter; // shortcut to meshFilter
+	Vector3[] originalVertices; // once we start deforming the mesh, we won't remember it
+
+	public float oceanHeight = 1f; // amplitude
+	public float oceanSpeed = 1f; // frequency
+
+	// Use this for initialization
+	void Start () {
+		// caching the shortcut
+		mFilter = GetComponent<MeshFilter>();
+		// cloning a pristine copy of our mesh for later
+		originalVertices = mFilter.mesh.vertices.Clone() as Vector3[];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// make a new copy of the vertices to deform
+		Vector3[] newVertices = originalVertices.Clone() as Vector3[];
+
+		// go through the vertex array and do stuff to each vertex
+		for ( int i=0; i<newVertices.Length; i++ ) {
+			newVertices[i] += new Vector3( 0f, Mathf.Sin( (Time.time + i) * oceanSpeed) * oceanHeight, 0f );
+		}
+
+		// put the vertices back into the mesh
+		Mesh deformedMesh = new Mesh();
+		deformedMesh.vertices = newVertices; // NEW VERTICES!
+		deformedMesh.triangles = mFilter.mesh.triangles; // old triangles
+		deformedMesh.uv = mFilter.mesh.uv; // copy old UVs
+		// deformedMesh.normals = mFilter.mesh.normals; // copy old normals
+		deformedMesh.tangents = mFilter.mesh.tangents; // copy old tangents
+
+		deformedMesh.RecalculateNormals(); // recalculate normals
+
+		// put the mesh back into the mesh filter
+		mFilter.mesh = deformedMesh;
+
+		// put the mesh into the Mesh Collider too!
+		GetComponent<MeshCollider>().sharedMesh = deformedMesh;
+
+	}
+}
+
+
+
+
+
---
+++ week11/Assets/scripts/WaterMeshDeform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterMeshDeform : MonoBehaviour {
+	
+	Vector3[] baseVertices; // variable to remember base vertex positions
+	MeshFilter meshFilter;
+	
+	public float waveHeight = 0.5f;
+	public float waveFreqDirection = 0.5f;
+	public float waveSpeed = 2f;
+	
+	// Use this for initialization
+	void Start () {
+		meshFilter = GetComponent<MeshFilter>();
+		baseVertices = meshFilter.mesh.vertices.Clone() as Vector3[];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// start with a fresh clean copy
+		Vector3[] newVertices = baseVertices.Clone() as Vector3[];
+		
+		// apply sin wave to vertices
+		for ( int i=0; i<newVertices.Length; i++ ) {
+			newVertices[i] += Vector3.up * Mathf.Sin( (Time.time * waveSpeed + i) * waveFreqDirection) * waveHeight;
+		}
+		
+		// make a new "Mesh" data container
+		Mesh deformedMesh = new Mesh();
+		deformedMesh.vertices = newVertices;
+		deformedMesh.triangles = meshFilter.mesh.triangles;
+		deformedMesh.uv = meshFilter.mesh.uv;
+		deformedMesh.tangents = meshFilter.mesh.tangents;
+		deformedMesh.RecalculateNormals ();
+		
+		// plug meshes back into components
+		meshFilter.mesh = deformedMesh;
+		GetComponent<MeshCollider>().sharedMesh = deformedMesh;
+		
+	} // closes out Update()
+} // closes out the class
